759cdf9974ec2fd6caacdfe7107798e902fb77a7
vishal yadav <115334069+vishalyadav2816@users.noreply.github.com>
2023-02-02 22:48:30 +0530

    Area of a circle

---
+++ areacircle.c
@@ -0,0 +1,19 @@
+//area of a circle
+
+#include<stdio.h>
+
+int main()
+{
+	float a,r;
+	printf("Enter the r of the circle\n");
+	scanf("%f",&r);
+	
+	a=2*3.14*r;
+	
+	printf("The area of the circle is %f",a);
+	
+	return 0;
+}
+
+
+
