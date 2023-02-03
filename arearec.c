abb710581e69de4a0f7739e0d00926caea595d27
vishal yadav <115334069+vishalyadav2816@users.noreply.github.com>
2023-02-03 18:40:15 +0530

    area of a rectangle

---
+++ arearec.c
@@ -0,0 +1,17 @@
+// area of a rectangle
+
+#include<stdio.h>
+
+int main()
+{
+	float l,b,a;
+	printf("enter the length \n",l);
+	scanf("%f",&l);
+	
+	printf("entet the breadth \n",b);
+	scanf("%f",&b);
+	
+	a=(l*b);
+	
+	printf("The area of the rectangle is %f",a);
+}
