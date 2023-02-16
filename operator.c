09f2342901dd9083e5eb1ca84c0af77326829b1d
vishal yadav <115334069+vishalyadav2816@users.noreply.github.com>
2023-02-16 23:10:41 +0530

    operator

---
+++ operator.c
@@ -0,0 +1,134 @@
+// operator
+
+#include<stdio.h>
+int main()
+//{
+//	int a=2,b;
+//	b=++a; // a=3,b=3
+//	printf("a and b %d %d \n",a,b);
+//	
+//	int x=5,y;
+//	y=x++; // y=5,x=6
+//	printf("x and y %d %d",x,y);
+//}
+
+
+// relatinal operator
+// <,>,<=,=>,==,!=
+
+
+//{
+//	int a=12,b=45,com;
+//	com=a>b;
+//	printf("a>b %d\n",com);
+//	com=a<b;
+//	printf("a<b %d\n",com);
+//	com=a!=12;
+//	printf("a!=12 %d\n",com);
+//	com=a!=b;
+//	printf("a!=b %d",com);
+//	
+//}
+	
+	
+	
+	
+	
+// logical operators
+// AND - &&
+// OR -  ||
+// NOT -  !
+
+//{
+//	int a=12,b=45,c=67,d=89,com;
+//	com = a<b && c<d;
+//	printf("com is %d \n",com);//1
+//	
+//	com = a>b && c<d;
+//	printf("com is %d \n",com);//0
+//	
+//	com = a>b || c<d;
+//	printf("com is %d \n",com);//1
+//	
+//	com = !(a>b || c<d);
+//	printf("com is %d",com);//0
+//}
+
+//{
+//	int x;
+//	x= 7 && 0;
+//	printf(" x = %d",x);
+//	x= !('A');
+//	printf("\n x is %d",x);
+//	x = 3.14 || 2.34;
+//	printf("\n x is %d",x);
+//	x = !(3.14) || 2.34;
+//	printf("\n x is %d",x);
+//	
+//}
+
+
+// Bitwise operator
+// Bitwise AND &, bitwise OR |, bitwise XOR 
+
+{
+	int a=11,b=6,c;
+	c = a & b ;
+	printf("c is %d \n",c);
+	
+	int x=5,y=8,z;
+	z=x|y;
+	printf("z is %d",z);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
