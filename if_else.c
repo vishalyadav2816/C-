e06b5ab316d7528033badee43b0b6f9a4bfc2769
vishal yadav <115334069+vishalyadav2816@users.noreply.github.com>
2023-02-17 23:06:01 +0530

    if else

---
+++ if_else.c
@@ -0,0 +1,14 @@
+// if else statement
+# include <stdio.h>
+int main ()
+{
+	int age;
+	printf("Enter the age of person \n");
+	scanf("%d",&age);
+	if (age>=18)
+	printf("you are elgilble to vote\n");
+	else
+	printf("you are not elgible to vote");
+	
+	printf("end of the program");
+}
