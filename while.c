b1330249b73e281e2570662cdcb12f4b6d3c1f0c
vishal yadav <115334069+vishalyadav2816@users.noreply.github.com>
2023-02-18 11:40:33 +0530

    while loop

---
+++ while.c
@@ -0,0 +1,70 @@
+// control statement 
+// loops
+//while loop
+// print  a message 10 times on the screen
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int i=0;
+//	while(i<10)
+//	{
+//		printf("hello world\n");
+//		i++;
+//		}	
+//}
+
+// print  1 to 10 using while loop
+
+//#include<stdio.h>
+//int main()
+//
+//{
+//	int x=1;
+//	while(x<=10)
+//	{
+//		printf("%d \n",x);
+//		++x;
+//	}
+//}
+
+
+
+////to print a mesage using for loop
+//#include<stdio.h>
+//int main()
+//{
+//	int x;
+//	for(x=1;x<=10;x++)
+//	{
+//		printf("hello world \n");
+//	}
+//}
+
+
+
+//to print 10 to 1 using for loop
+#include<stdio.h>
+int main()
+
+{
+	int x;
+	for(x=10;x>=1;x--)
+	{
+		printf("%d \n",x);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
